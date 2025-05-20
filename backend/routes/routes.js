a353a9a959536993a203b890fa1744f85c2cdfad
Tareq-dev <tareque.dev@gmail.com>
2025-05-20 13:25:21 +0600

    created Teacher API

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { getAllStudents, getStudentById , createStudent, updateStudents, deleteStudent} from "../controllers/studentRoutes.js";
+import { getAllStudents, getStudentById, createStudent, updateStudents, deleteStudent } from "../controllers/studentRoutes.js";
+import { getAllteachers } from "../controllers/teacherRoutes.js";
 
 const router = express.Router();
 
@@ -9,5 +10,12 @@ router.get("/student/:id", getStudentById);
 router.post('/student', createStudent);
 router.put('/student/:id', updateStudents);
 router.delete('/student/:id', deleteStudent);
+
+//teachers Routes
+router.get("/teachers", getAllteachers)
+
+
+
+
 
 export default router;
